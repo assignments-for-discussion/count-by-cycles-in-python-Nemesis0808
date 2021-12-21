b4626e37664dd3bff3170210f198553c282b83af
Nithin Iyer k s <94003103+Nemesis0808@users.noreply.github.com>
2021-12-21 13:07:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 
 def count_batteries_by_usage(cycles):
-counts={'lowCount':0,'mediumCount':0,'highCount':0}
+    counts={'lowCount':0,'mediumCount':0,'highCount':0}
     for cycle in cycles:
         if cycle<150:
             counts['lowCount']+=1
